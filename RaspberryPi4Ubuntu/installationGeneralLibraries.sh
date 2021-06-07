9c473d58e0e884e09ae7e704f044f2b63374d7c6
zarbokk <timhansen93@googlemail.com>
2021-06-07 17:18:58 +0200

    initial commit

--- RaspberryPi4Ubuntu/installationGeneralLibraries.sh
+++ RaspberryPi4Ubuntu/installationGeneralLibraries.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install libpcl-dev -y
+sudo apt install ros-noetic-pcl* -y
+
 sudo apt install libopencv-dev python3-opencv -y
-sudo apt install -y libfftw3-bin -y
+sudo apt install -y libfftw3-dev -y
