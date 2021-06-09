251ccc4c575fc43875f2174c3934f03ae15a3928
zarbokk <timhansen93@googlemail.com>
2021-06-09 14:12:32 +0200

    update slam scripts

--- RaspberryPi4Ubuntu/installationUnderwaterSlamPackages.sh
+++ RaspberryPi4Ubuntu/installationUnderwaterSlamPackages.sh
@@ -4,6 +4,7 @@ cd ~/catkin_ws/src
 
 git clone https://github.com/Zarbokk/BlueROV2Common.git
 git clone https://github.com/Zarbokk/UnderwaterSlam.git
+git clone https://github.com/Zarbokk/soft20.git
 
 cd ~/catkin_ws
 catkin build
