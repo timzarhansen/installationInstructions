7f9d3b2f94afdbc16bc2b8ba71f872bc33cceabb
tim <timhansen93@googlemail.com>
2021-10-26 14:31:56 +0200

    update

--- RaspberryPi4Ubuntu/installationUnderwaterSlamPackages.sh
+++ RaspberryPi4Ubuntu/installationUnderwaterSlamPackages.sh
@@ -22,6 +22,7 @@ git clone https://github.com/Zarbokk/BlueROV2Common.git
 git clone https://github.com/Zarbokk/UnderwaterSlam.git
 git clone https://github.com/Zarbokk/soft20.git
 git clone https://github.com/Zarbokk/ping360_sonar.git
+git clone https://github.com/Zarbokk/dvl-a50-ros-driver
 
 
 
