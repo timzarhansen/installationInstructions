92f0682e1c896ce23b3ef0851c4fc34d294a5ebc
tim-linux <t.hansen@jacobs-university.de>
2021-12-01 15:53:13 +0100

    update

--- ubuntu20_04Start/installationUnderwaterSlamPackages.sh
+++ ubuntu20_04Start/installationUnderwaterSlamPackages.sh
@@ -7,6 +7,7 @@ git clone https://github.com/Zarbokk/BlueROV2Common.git
 git clone https://github.com/Zarbokk/UnderwaterSlam.git
 git clone https://github.com/Zarbokk/soft20.git
 git clone https://github.com/CentraleNantesRobotics/ping360_sonar.git
+git clone https://github.com/GSO-soslab/waterlinked_dvl_ros
 
 cd ~/catkin_ws
 catkin build
