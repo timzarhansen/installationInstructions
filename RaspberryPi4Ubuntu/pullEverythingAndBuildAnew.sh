f47b9eed462f8624094db90b7b3775d8d52cd9f2
tim-linux <t.hansen@jacobs-university.de>
2022-03-03 14:33:56 +0100

    added message and service package

--- RaspberryPi4Ubuntu/pullEverythingAndBuildAnew.sh
+++ RaspberryPi4Ubuntu/pullEverythingAndBuildAnew.sh
@@ -8,6 +8,12 @@ cd ~/catkin_ws/src/soft20
 git pull
 cd ~/catkin_ws/src/ping360_sonar
 git pull
+cd ~/catkin_ws/src/commonBlueROVMSG
+git pull
+cd ~/catkin_ws/src/waterlinked_dvl_ros
+git pull
+
+
 
 cd ~/catkin_ws
 catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release
