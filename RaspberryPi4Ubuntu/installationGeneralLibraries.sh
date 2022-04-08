7162dff0975cc74037ba41d5e3ec10b81535efe3
Tim <t.hansen@jacobs-university.de>
2022-04-08 11:28:42 +0200

    Update installationGeneralLibraries.sh
    changed libcgal

--- RaspberryPi4Ubuntu/installationGeneralLibraries.sh
+++ RaspberryPi4Ubuntu/installationGeneralLibraries.sh
@@ -17,7 +17,7 @@ sudo apt install python3-pip -y
 sudo apt install python3-pigpio -y
 sudo apt install arm-none-eabi-gcc -y #for compiling px4 on rpi4 needs 2.3gb space
 sudo apt install libqt5gamepad5* -y
-sudo apt install libgcal-dev -y
+sudo apt install libcgal-dev -y
 
 pip3 install --user packaging
 pip3 install --user toml
