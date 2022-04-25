4548369ac863f7b5b0837e1b256e9c41357e0d12
zarbokk <timhansen93@googlemail.com>
2022-04-25 16:39:18 +0200

    corrected ip addresses and added copy mode to pi

--- developmentTools/copyROSSoftwareToRPI4.sh
+++ developmentTools/copyROSSoftwareToRPI4.sh
@@ -5,7 +5,16 @@ copyFolder () {
   rsync -avh ~/catkin_ws/src/$2/ ubuntu@$1:~/catkin_ws/src/$2 --exclude=cmake-build-debug --exclude=build --exclude=.git --exclude=.idea
 }
 
-IP_ADDRESS="10.42.0.135"
+IP_ADDRESS="10.42.0.150"
+
+copyFolder "$IP_ADDRESS" "BlueROV2Common"
+copyFolder "$IP_ADDRESS" "UnderwaterSlam"
+copyFolder "$IP_ADDRESS" "soft20"
+copyFolder "$IP_ADDRESS" "ping360_sonar"
+copyFolder "$IP_ADDRESS" "commonBlueROVMSG"
+copyFolder "$IP_ADDRESS" "waterlinked_dvl_ros"
+
+IP_ADDRESS="10.42.0.181"
 
 copyFolder "$IP_ADDRESS" "BlueROV2Common"
 copyFolder "$IP_ADDRESS" "UnderwaterSlam"
