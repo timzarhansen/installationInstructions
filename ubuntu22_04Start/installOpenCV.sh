fec0d278de5011d2181ad96caea1df00c800ea04
tim-external <t.hansen@jacobs-university.de>
2023-11-28 15:15:47 +0100

    added enable non free stuff

--- ubuntu22_04Start/installOpenCV.sh
+++ ubuntu22_04Start/installOpenCV.sh
@@ -12,8 +12,8 @@ git clone https://github.com/opencv/opencv.git
 git clone https://github.com/opencv/opencv_contrib.git
 mkdir build
 cd build
-cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ../opencv
-cmake --build .
+cmake -DOPENCV_ENABLE_NONFREE:BOOL=ON -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ../opencv
+cmake --build . --config Release -- -j 10
 sudo make install
 
 
